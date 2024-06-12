6f9c58e57bf6d92ca12994993e4789b9f5c1078a
Ajay Singh <ajayoas36ty@gmail.com>
2024-06-12 16:37:06 +0530

    basic implementation of the components and their functionalities

--- expense-tracker/src/App.vue
+++ expense-tracker/src/App.vue
@@ -2,30 +2,34 @@
   <div>
     <Header />
     <div class="container">
-      <Balance />
+      <Balance :total="total" />
       <income-expenses />
-      <transaction-list />
+      <transaction-list :transactions="transactions" />
       <add-transaction />
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction,
-  },
-};
+import { ref, computed } from "vue";
+
+const transactions = ref([
+  { id: 1, text: "Salary", amount: 7500000 },
+  { id: 1, text: "Plant", amount: -4490.85 },
+  { id: 1, text: "Book", amount: 91542.1 },
+  { id: 1, text: "Liquor", amount: -87518 },
+]);
+const total = computed(() => {
+  return transactions.value.reduce((acc, trans) => {
+    return acc + trans.amount;
+  }, 0);
+});
 </script>
 
 <style scoped></style>
--- expense-tracker/src/components/TransactionList.vue
+++ expense-tracker/src/components/TransactionList.vue
@@ -2,12 +2,23 @@
   <div>
     <h3>History</h3>
     <ul id="list" class="list">
-      <li class="minus">
-        Cash <span>-$400</span><button class="delete-btn">x</button>
-      </li>
-      <li class="plus">
-        Paycheck <span>$800</span><button class="delete-btn">x</button>
+      <li
+        v-for="trans in transactions"
+        :key="trans.id"
+        :class="trans.amount < 0 ? 'minus' : 'plus'"
+      >
+        {{ trans.text }} <span>${{ trans.amount }}</span
+        ><button class="delete-btn">x</button>
       </li>
     </ul>
   </div>
 </template>
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
